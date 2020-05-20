52404fa34c2aba18b68e4c8280733b99b1ad55b5
Isma <didouhismael@gmail.com>
2020-05-20 22:45:43 +0200

    New template engine module

--- functions/index.js
+++ functions/index.js
@@ -1,26 +1,8 @@
 const functions = require("firebase-functions");
 const express = require("express");
-
 const fs = require("fs");
 
-function replaceTemplate(template, product) {
-  // with the /{%NAME%}/g we make the replace global and it will replace all occurrences
-  let output = template
-    .replace(/{%ID%}/g, product.id)
-    .replace(/{%NAME%}/g, product.name)
-    .replace(/{%IMAGE%}/g, product.image)
-    .replace(/{%FROM%}/g, product.from)
-    .replace(/{%NUTRIENTS%}/g, product.nutrients)
-    .replace(/{%QUANTITY%}/g, product.quantity)
-    .replace(/{%PRICE%}/g, product.price)
-    .replace(/{%DESCRIPTION%}/g, product.description);
-
-  // Add non-organic class when prop organic is false
-  if (!product.organic)
-    output = output.replace(/{%NOT_ORGANIC%}/g, "not-organic");
-
-  return output;
-}
+const templateEngine = require("./modules/templateEngine");
 
 /** Load data */
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
@@ -41,9 +23,7 @@ const app = express();
 
 app.get("/", (req, res) => {
   // Replace all vars in all cards and join result so we can print it to the html
-  const cards = jsonData
-    .map((item) => replaceTemplate(cardView, item))
-    .join("");
+  const cards = jsonData.map((item) => templateEngine(cardView, item)).join("");
 
   // Replace cards placeholder for the partial
   const output = overviewView.replace("{%PRODUCT_CARDS%}", cards);
@@ -53,9 +33,7 @@ app.get("/", (req, res) => {
 });
 app.get("/overview", (req, res) => {
   // Replace all vars in all cards and join result so we can print it to the html
-  const cards = jsonData
-    .map((item) => replaceTemplate(cardView, item))
-    .join("");
+  const cards = jsonData.map((item) => templateEngine(cardView, item)).join("");
 
   // Replace cards placeholder for the partial
   const output = overviewView.replace("{%PRODUCT_CARDS%}", cards);
@@ -66,7 +44,7 @@ app.get("/overview", (req, res) => {
 
 app.get("/product/:id", (req, res) => {
   const product = jsonData[parseInt(req.params.id, 10)];
-  const output = replaceTemplate(productView, product);
+  const output = templateEngine(productView, product);
 
   res.set("Cache-Control", "public, max-age=31557600, s-maxage=31557600");
   res.send(output);
